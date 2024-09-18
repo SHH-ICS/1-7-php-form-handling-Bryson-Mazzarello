c8c37ff79772d224edd965703056d87c982a5bc1
Bryson-Mazzarello <bryson.mazzarello@stu.ocsb.ca>
2024-09-18 09:51:43 -0400

    Update and rename index.html to index.php
    We worked

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>Hello O.G</title>    
+  </head>
+  
+  <body>
+
+    <p>Would you mind running those pockets</p>
+    
+    <php>
+      
+      <form action="welcome_get.php" method="_POST">
+        Name: <input type="text" name="userName"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+      </form>
+      
+    </php>
+    
+  </body>
+  
+</html>
