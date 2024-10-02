aa93c783f261fe2646bcf765b7deebe6123d9400
Bryson-Mazzarello <bryson.mazzarello@stu.ocsb.ca>
2024-10-02 10:35:37 -0400

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,73 @@
+<html>
+  <head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+      <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+      <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+      <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+      <link rel="manifest" href="./site.webmanifest">
+      <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#5bbad5">
+      <meta name="msapplication-TileColor" content="#da532c">
+      <meta name="theme-color" content="#ffffff">
+      <meta name="viewport" content="width = device-width, initial-scale = 1">
+      <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+      <link rel="manifest" href="./site.webmanifest">
+      </div>
+    </form>
+    <link rel="stylesheet" href="styles.css">
+    <title>Diabetis</title>
+  </head>
+
+  <body>
+      <!-- Left aligned menu below button -->
+      <button id="demo-menu-lower-left"
+        class="mdl-button mdl-js-button mdl-button--icon">
+        <i class="material-icons">more_vert</i>
+      </button>
+      <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect"
+        for="demo-menu-lower-left">
+        <a href="/1-5-mdl-html-page-Bryson-Mazzarello/index.html" class="mdl-menu__item">Back to main page</a>
+        <li class="mdl-menu__item mdl-menu__item--full-bleed-divider">Another Action</li>
+        <li class="mdl-menu__item mdl-menu__item--full-bleed-divider">Another Action</li>
+        <li class="mdl-menu__item">Yet Another Action</li>
+      </ul>
+    <!-- Header -->
+      <h1>Hello, World!</h1>
+<!-- Simple list -->
+<style>
+.demo-list-item {
+  width: 300px;
+}
+</style>
+
+<ul class="demo-list-item mdl-list">
+  <li class="mdl-list__item">
+    <span class="mdl-list__item-primary-content">
+      This is my list demo
+    </span>
+  </li>
+  <li class="mdl-list__item">
+    <span class="mdl-list__item-primary-content">
+      You better like it
+    </span>
+  </li>
+  <li class="mdl-list__item">
+    <span class="mdl-list__item-primary-content">
+      4+ fr fr
+    </span>
+  </li>
+  <li class="mdl-list__item">
+   <span class="mdl-list__item-primary-content">
+    Top ten raisins why I am the G.O.A.T
+   </span>
+  </li>
+</ul>
+    <div id="hello-world">Some text here(I DIDN'T COPY AND PASTE THIS TRUSTTTTT.</div>
+<!-- Default Slider -->
+<input class="mdl-slider mdl-js-slider" type="range"
+  min="0" max="100" value="0" tabindex="0">
+  </body>
+</html>
