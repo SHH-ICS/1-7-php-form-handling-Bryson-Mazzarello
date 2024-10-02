dc691d580c0707feaba1b1e25ea7777a3055aa24
Bryson-Mazzarello <bryson.mazzarello@stu.ocsb.ca>
2024-10-02 10:31:10 -0400

    Update handler.php

--- handler.php
+++ handler.php
@@ -16,9 +16,6 @@
       <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
       <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
       <link rel="manifest" href="./site.webmanifest">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <title>Welcome Page</title>
   </head>
 
